3b043ef3b314d5dacc0f35bcc0e473653c49e80f
Evgenii Tolmachev <zoomex95@gmail.com>
2020-11-26 19:30:43 +0300

    gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
-import com.google.*;
 import com.google.gson.Gson;
 
+
 public class Main {
+
     public static void main(String[] args) {
         ArrayList<Companies> listOfCompanies = new ArrayList<>();
-/*        Actives actives = new Actives(123,2020,"lkn",Valutes.RUB);
-        Company company = new Company("luk", "msc", "111", "222", "333",
-                                        actives);
-        System.out.println(company);*/
+        //Actives actives = new Actives("123","2020","lkn",Valutes.RUB);
+        //Company company = new Company("luk", "msc", "111", "222", "333",
+                                        //actives);
+        //System.out.println(company);
+
         Gson gson = new Gson();
-        Company company = gson.fromJson("H:\\File.json",Company.class);
+
+
+        Company company = gson.fromJson("H:\\File.txt", Company.class);
         System.out.println(company);
 
 
